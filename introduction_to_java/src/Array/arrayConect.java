5e25ad83944fdcf8f28df9c628acc41305c5dc02
Hai <hai@gmail.com>
2022-10-17 18:56:36 +0700

    add lesson all

--- introduction_to_java/src/Array/arrayConect.java
+++ introduction_to_java/src/Array/arrayConect.java
@@ -9,24 +9,25 @@ public class ArrayConect {
         int size;
         Scanner scanner = new Scanner(System.in);
 //        Gọi phương thức createArr để tạo mảng
-//        createArr(arrA);
-        do {
-            System.out.println("Enter a size array A: ");
-            size = scanner.nextInt();
-            if (size>20){
-                System.out.println("Size does not exceed 20");
-            }
-        } while (size>20);
-//      Gán giá trị cho arrA
-        arrA = createArr(size);
-        do {
-            System.out.println("Enter a size array B: ");
-            size = scanner.nextInt();
-            if (size>20){
-                System.out.println("Size does not exceed 20");
-            }
-        } while (size>20);
-        arrB = createArr(size);
+        createArr(arrA);
+        createArr(arrB);
+//        do {
+//            System.out.println("Enter a size array A: ");
+//            size = scanner.nextInt();
+//            if (size>20){
+//                System.out.println("Size does not exceed 20");
+//            }
+//        } while (size>20);
+////      Gán giá trị cho arrA
+//        arrA = createArr(size);
+//        do {
+//            System.out.println("Enter a size array B: ");
+//            size = scanner.nextInt();
+//            if (size>20){
+//                System.out.println("Size does not exceed 20");
+//            }
+//        } while (size>20);
+//        arrB = createArr(size);
         int[] arrC = new int[arrA.length + arrB.length];
         int num = 0;
         for (int i = 0; i < arrC.length; i++) {
@@ -41,17 +42,17 @@ public class ArrayConect {
         }
     }
 //    Phương thức tạo một mảng
-    public static int[] createArr(int size){
-//      int size;
-        int[] array;
+    public static int[] createArr(int[] array){
+      int size;
+//        int[] array;
         Scanner scanner = new Scanner(System.in);
-//        do {
-//            System.out.println("Enter a size: ");
-//            size = scanner.nextInt();
-//            if (size>20){
-//                System.out.println("Size does not exceed 20");
-//            }
-//        } while (size>20);
+        do {
+            System.out.println("Enter a size: ");
+            size = scanner.nextInt();
+            if (size>20){
+                System.out.println("Size does not exceed 20");
+            }
+        } while (size>20);
         array = new int[size];
         for (int i = 0; i < size; i++) {
             System.out.print("Enter element " + (i + 1) + ": ");
